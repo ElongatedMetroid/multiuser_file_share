a414a2f1202ef4399c140ff1f49609f1a052e828
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-12-14 18:55:04 -0800

    add client and server basics

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpStream;
+
+use lib_mfs::MfsStreamWriter;
+
 fn main() {
-    println!("Hello, world!");
+    let stream = TcpStream::connect("127.0.0.1:6969").unwrap();
+
+    handle_stream(stream);
+}
+
+fn handle_stream(mut stream: TcpStream) {
+    MfsStreamWriter::write(&mut stream, &String::from("Hello World8888")).unwrap();
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,19 @@
+use std::net::{TcpListener, TcpStream};
+
+use lib_mfs::MfsStreamReader;
+use rayon::ThreadPoolBuilder;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
+    let pool = ThreadPoolBuilder::new().num_threads(8).build().unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.install(|| handle_client(stream));
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    println!("{}", MfsStreamReader::read::<String>(&mut stream).unwrap());
 }
