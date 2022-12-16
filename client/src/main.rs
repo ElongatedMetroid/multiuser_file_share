2eca098a753ef2998697561260db2047600431b8
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-12-15 18:00:07 -0800

    recieve max_data_size and break_up_data

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,6 @@
 use std::net::TcpStream;
 
-use lib_mfs::MfsStreamWriter;
+use lib_mfs::{reader::MfsStreamReader, user::MfsUser, writer::MfsStreamWriter};
 
 fn main() {
     let stream = TcpStream::connect("127.0.0.1:6969").unwrap();
@@ -9,5 +9,17 @@ fn main() {
 }
 
 fn handle_stream(mut stream: TcpStream) {
-    MfsStreamWriter::write(&mut stream, &String::from("Hello World8888")).unwrap();
+    let mut reader = MfsStreamReader::new(8, false);
+    let writer = MfsStreamWriter::new(
+        reader.read::<u64>(&mut stream).unwrap(),
+        reader.read::<bool>(&mut stream).unwrap(),
+    );
+    reader.set_max_data_size(writer.max_data_size());
+    reader.set_break_up_data(writer.break_up_data());
+
+    dbg!(writer.max_data_size(), writer.break_up_data());
+
+    writer
+        .write(&mut stream, &MfsUser::new("bob", "12345"))
+        .unwrap();
 }
